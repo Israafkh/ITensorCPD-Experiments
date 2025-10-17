930fdb68d935ed3b2974c6e29052be68716a5817
Karl Pierce <karl.m.pierce@gmail.com>
2025-10-17 15:24:06 -0400

    Small comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using Pkg
-Pkg.develop(path = "$(@__DIR__)/../../ITensorCPD.jl")
+## I don't have this path to add. you could add
+# Pkg.develop(url="github.com/kmp5VT/ITensorCPD.jl")
+## and tell people to uncomment to add ITensorCPD
+#Pkg.develop(path = "$(@__DIR__)/../../ITensorCPD.jl")
 
 using ITensorCPD
 using ITensors
