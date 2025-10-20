0886e62b17c311cf964afd5d1632bdce7e8ed93f
Karl Pierce <karl.m.pierce@gmail.com>
2025-10-20 11:24:30 -0400

    Small update for performance

--- test/well_active_d.jl
+++ test/well_active_d.jl
@@ -25,7 +25,7 @@ end
 path = "$(@__DIR__)/datasets/active_matter/data/train/"
 files = readdir(path)
 dat = Array{Float32}(undef, 256, 256, 81);
-i = 1
+ i = 1
 #for i in 1:45
     file = h5open(path*files[i])
     press = file["t2_fields"]["D"];
@@ -36,39 +36,39 @@ i = 1
 nsteps = size(dat)[end]
 A = ITensors.itensor(dat, Index.(size(dat)))
 println(size(A))
+ranks = [1,10,20,50,100,200]
+
+verbose = true
+cp_A = ITensorCPD.random_CPD(A, 1, rng=RandomDevice())
+
+check = ITensorCPD.CPDiffCheck(1e-6, 50)
+alg = ITensorCPD.SEQRCSPivProjected((1,1,1,1), (400,400,400,400), (1,2,3,4), (10,10,10,10))
+alsQR = ITensorCPD.compute_als(A, cp_A; alg, check);
+
+alg = ITensorCPD.LevScoreSampled((400,))
+alsLev = ITensorCPD.compute_als(A, cp_A; alg, check);
+
 error_SEQRCS = Vector{Float64}()
 error_leverage = Vector{Float64}()
 error_direct = Vector{Float64}()
-ranks = [1,2,3,5,10,15,30,50]
 for rk in ranks
     r = Index(rk, "CP_rank")
-    check = ITensorCPD.FitCheck(1e-4, 50,sqrt(sum(A.^2)) )
-    verbose = true
+    check = ITensorCPD.FitCheck(1e-3, 50, norm(A) )
     cp_A = ITensorCPD.random_CPD(A, r, rng=RandomDevice())
-    als = ITensorCPD.compute_als(A, cp_A; alg=ITensorCPD.direct(), check);
 
-     opt_A = ITensorCPD.optimize(cp_A, als; verbose);
-    #direct_error =  norm(A - ITensorCPD.reconstruct(opt_A)) / sqrt(sum(A.^2)) 
-     direct_error = check_fit(als, opt_A.factors, r, opt_A.λ, 1)
-     push!(error_direct,direct_error)
+    alsNormal = ITensorCPD.compute_als(A, cp_A; alg=ITensorCPD.direct(), check);
+    opt_A = ITensorCPD.optimize(cp_A, alsNormal; verbose);
+    direct_error = check_fit(alsNormal, opt_A.factors, r, opt_A.λ, 1)
+    push!(error_direct,direct_error)
 
-    check = ITensorCPD.CPDiffCheck(1e-7, 50)
-
-    alg = ITensorCPD.SEQRCSPivProjected((1,1,1,1), (200,200,200,200), (1,2,3,4), (10,10,10,10))
-    als = ITensorCPD.compute_als(A, cp_A; alg, check);
-    #  int_opt_A =
-    # ITensorCPD.als_optimize(A, cp_A; alg, verbose);
-    int_opt_A = ITensorCPD.optimize(cp_A, als; verbose);
-    #SEQRCS_error =   norm(A - ITensorCPD.reconstruct(int_opt_A)) / sqrt(sum(A.^2))
-    SEQRCS_error = check_fit(als, int_opt_A.factors, r, int_opt_A.λ, 1)
+    int_opt_A = ITensorCPD.optimize(cp_A, alsQR; verbose);
+    SEQRCS_error = check_fit(alsQR, int_opt_A.factors, r, int_opt_A.λ, 1)
     println("result for active using SEQRCS: ",SEQRCS_error)
     push!(error_SEQRCS,SEQRCS_error)
     
     
-    alg = ITensorCPD.LevScoreSampled((200,))
-    lev_opt_A = ITensorCPD.als_optimize(A, cp_A; alg, check, verbose);
-    #lev_error =   norm(A - ITensorCPD.reconstruct(int_opt_A)) / norm(A) 
-    lev_error = check_fit(als, lev_opt_A.factors, r, lev_opt_A.λ, 1)
+    lev_opt_A = ITensorCPD.optimize(cp_A, alsLev; verbose);
+    lev_error = check_fit(alsLev, lev_opt_A.factors, r, lev_opt_A.λ, 1)
     println("result for active using leverage method is ",lev_error)
     push!(error_leverage,lev_error)
     
