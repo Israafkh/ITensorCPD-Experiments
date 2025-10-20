b33ee37b59b55d2e0ae83d7b79144c36f5fa9782
Karl Pierce <karl.m.pierce@gmail.com>
2025-10-20 13:17:56 -0400

    Small update for perturbed solution.

--- test/solver_stability/pert_solution.jl
+++ test/solver_stability/pert_solution.jl
@@ -1,5 +1,4 @@
-using ITensors, ITensorCPD
-using Revise
+include("$(@__DIR__)/../test_env.jl")
 using ITensorCPD: compute_krp, matricize_tensor, solve_ls_problem, row_norm, post_solve, had_contract
 
 ## This looks at the stability of the SEQRCS method for near
@@ -25,10 +24,10 @@ function single_solve(alsRef, cpd, fact)
 end
 
 ## We should try for higher order tensors too
-i, j, k  = Index.((100,110,120));
+is  = Index.((10,20,30,40));
 
-T = ITensor(Float64, i,j,k);
-cpd_exact = ITensorCPD.random_CPD(T, 201);
+T = ITensor(Float64, is);
+cpd_exact = ITensorCPD.random_CPD(T, 15);
 T = ITensorCPD.reconstruct(cpd_exact);
 
 check=ITensorCPD.FitCheck(0, 100, norm(T));
@@ -36,36 +35,60 @@ check=ITensorCPD.FitCheck(0, 100, norm(T));
 #### exact solution
 alg = ITensorCPD.direct()
 al = ITensorCPD.compute_als(T, cpd_exact; alg, check);
-# ITensorCPD.optimize(cpd_exact, als; verbose=true);
 
 cpd_exact, als = single_solve(al, cpd_exact, 1);
 cpd_exact, als = single_solve(als, cpd_exact, 2);
 cpd_exact, als = single_solve(als, cpd_exact, 3);
+cpd_exact, als = single_solve(als, cpd_exact, 4);
 
 
 #### SEQRCS check
-alpha = .01
+alpha = .1
+mode_opt = 4
 LevFits = Vector{Vector{Float64}}()
-for samps in [210]
+for samps in [50,100,200,500,2000]
     fits = Vector{Float64}()
-    for blocks in 1:8
+    for blocks in 1:50
         cpd_pert = deepcopy(cpd_exact)
-        #alg = ITensorCPD.SEQRCSPivProjected(1, samps, (1,2,3), (10,10,10))
-        alg = ITensorCPD.BlockLevScoreSampled(samps, blocks)
+        alg = ITensorCPD.BlockLevScoreSampled(samps, 1)
 
         
         array(cpd_pert.factors[2]) .+= alpha .* randn(size(cpd_pert.factors[2]))
         cpd_pert.factors[2], _ = row_norm(cpd_pert[2], ind(cpd_pert, 2))
         als = ITensorCPD.compute_als(T, cpd_pert; alg, check);
-        cpd_pert, als, fit = single_solve(als, cpd_pert, 1);
+        cpd_pert, als, fit = single_solve(als, cpd_pert, mode_opt);
         push!(fits, fit)
     end
     push!(LevFits, fits)
 end
 
 using Plots
-plot(LevFits[1], marker=:circle)
-plot!(LevFits[2], marker=:circle)
+plot(LevFits[2], marker=:circle)
 plot!(LevFits[3], marker=:circle)
 plot!(LevFits[4], marker=:circle)
-plot!(LevFits[6], marker=:circle)
+plot!(LevFits[5], marker=:circle)
+
+
+QRFits = Vector{Vector{Float64}}()
+for samps in [50,100,200,500,2000]
+    fits = Vector{Float64}()
+    for blocks in 1:50
+        cpd_pert = deepcopy(cpd_exact)
+        alg = ITensorCPD.SEQRCSPivProjected(1, samps, (1,2,3,4), (10,10,10,10))
+        #alg = ITensorCPD.BlockLevScoreSampled(samps, blocks)
+
+        
+        array(cpd_pert.factors[2]) .+= alpha .* randn(size(cpd_pert.factors[2]))
+        cpd_pert.factors[2], _ = row_norm(cpd_pert[2], ind(cpd_pert, 2))
+        als = ITensorCPD.compute_als(T, cpd_pert; alg, check);
+        cpd_pert, als, fit = single_solve(als, cpd_pert, mode_opt);
+        push!(fits, fit)
+    end
+    push!(QRFits, fits)
+end
+
+using Plots
+plot(QRFits[2], marker=:square)
+plot!(QRFits[3], marker=:square)
+plot!(QRFits[4], marker=:square)
+plot!(QRFits[5], marker=:square)
