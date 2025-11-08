bd87a7934b73e10b3c2ce6dcf59e2ef2013335fe
Karl Pierce <karl.m.pierce@gmail.com>
2025-11-08 13:45:04 -0500

    Update small issue in single solve

--- test/test_env.jl
+++ test/test_env.jl
@@ -33,8 +33,26 @@ function check_fit(als, factors, cprank, λ, fact)
     return fit
 end
 
+# Fit = 1 - || T - \hat{T} || / || T ||
+# sqrt(T^2 - 2 * T \hat{T} + \hat{T}^2)
+## Update this to give the error in the LS 
+function check_fit(als, factors, cprank, λ, fact)
+    target = als.target
+    inner_prod = (had_contract([target, dag.(factors)...], cprank) * dag(λ))[]
+    partial_gram = [fact * dag(prime(fact; tags=tags(cprank))) for fact in factors];
+    fact_square = ITensorCPD.norm_factors(partial_gram, λ)
+    ref_norm = sqrt(sum(target .^2))
+    normResidual =
+        sqrt(abs(ref_norm * ref_norm + fact_square - 2 * abs(inner_prod)))
+    fit = 1.0 - normResidual / norm(ref_norm)
+    println("CPD rank\tMode\tCPD Accuracy")
+    println("$(dim(cprank))\t\t$(fact)\t$(fit)")
+    return fit
+end
+
 # f(A) = || T - [[A, B, C]] ||^2
-# df/dA = -T (C \odot B) + A * (C \odot Base)^2
+# f(A) = sqrt(T^2 - 2 * T * [[A, B, C]] - [[A, B, C]]^2)
+# df/dA = -T (C \odot B) + A * (C \odot B)^2
 function check_grad(target, cpd, grammian, fact, updated_factor)
 
     factors = cpd.factors
@@ -52,7 +70,7 @@ function check_grad(target, cpd, grammian, fact, updated_factor)
     return loss_function
 end
 
-# f(A) = || T - A ( B \odot C) ||^2
+# f(A) = || T - [[A, B, C]]||^2
 function check_loss(target, cpd, grammian, fact, updated_factor)
     factors = cpd.factors
     cpr = ITensorCPD.cp_rank(cpd)
@@ -72,6 +90,31 @@ function check_loss(target, cpd, grammian, fact, updated_factor)
     println("CPD rank\tMode\tLoss")
     println("$(dim(cpr))\t\t$(fact)\t$(loss_function)")
     return loss_function
+end
+
+function single_solve(alsRef, cpd, fact)
+    cprank = ITensorCPD.cp_rank(cpd)
+    target_ind = ind(cpd, fact)
+    factors = deepcopy(cpd.factors);
+    als = deepcopy(alsRef)
+
+    gram = [i * prime(i, tags=tags(cprank)) for i in cpd.factors]
+    if haskey(alsRef.additional_items, :part_grammian)
+        als.additional_items[:part_grammian] .= gram
+    end
+
+    krp = compute_krp(als.mttkrp_alg, als, factors, cpd, cprank, fact)
+    mtkrp = matricize_tensor(als.mttkrp_alg, als, factors, cpd, cprank, fact)
+    solution = solve_ls_problem(als.mttkrp_alg, krp, mtkrp, cprank)
+    
+    factors[fact], λ = row_norm(solution, target_ind)
+    post_solve(als.mttkrp_alg, als, factors, λ, cpd, cprank, fact)
+
+    #fit = check_fit(als, factors, cprank, λ, fact)
+    fit = check_fit(alsRef, cpd.factors, cprank, cpd.λ, fact)
+    fit = check_grad(alsRef.target, cpd, gram, fact, solution)
+    fit = check_loss(alsRef.target, cpd, gram, fact, solution)
+    return ITensorCPD.CPD{ITensor}(factors, λ), als, fit
 end
 
 include("colinearity_tensor_generator.jl")
