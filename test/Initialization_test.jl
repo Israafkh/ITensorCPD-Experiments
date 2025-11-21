ab1632db1cf49b28b3e099a4b77ae44eb7d89c42
Israafkh <israa.fakih@epfl.ch>
2025-11-21 20:56:42 +0100

    Add initialization test

---
+++ test/Initialization_test.jl
@@ -0,0 +1,145 @@
+include("test_env.jl")
+using LinearAlgebra, Statistics, DataFrames, CategoricalArrays, StatsPlots
+## Generating a random tensor with CPD rank greater than dimension
+
+for elt in (Float32,Float64), c in [0.2, 0.8]
+    A, cp = Colinearity_Tensor(90, 3, (400,400,400), elt(c), nothing, elt)
+    ind_A = inds(A)  
+    dims = dim.(ind_A)
+    C = randn(dims...)
+    N = itensor(C,ind_A...)
+    T = A+ 0.1*(norm(A)/norm(N))*N
+    verbose= true
+    samples = [1, 50, 150, 300, 500, 1000, 1300,1500]
+    check_piv = ITensorCPD.CPDiffCheck(1e-5, 50)
+    check_direct = ITensorCPD.FitCheck(1e-5, 50,sqrt(sum(T.^2)) )
+    nrepeats = 1
+    for rk in [80,90,100]
+        rng=RandomDevice()
+        r = Index(rk, "CP_rank")
+        cp_T = ITensorCPD.random_CPD(T,r;rng)
+        
+        SEQRCS_error_vect = Vector{Float64}()
+        SEQRCS_error_vect_HOSVD = Vector{Float64}()
+        SEQRCS_error_vect_High = Vector{Float64}()
+        err_SEQRCS = Vector{Float64}()
+        err_SEQRCS_HOSVD = Vector{Float64}()
+        err_SEQRCS_High = Vector{Float64}()
+        ## SE-QRCS 
+        for s in samples
+            for q = 1:nrepeats
+                alg_QR =ITensorCPD.SEQRCSPivProjected(1, s, (1,2,3),(30,30,30))
+                alsQR = ITensorCPD.compute_als(T,cp_T;alg=alg_QR, check =check_piv);
+                # println("Start SEQRCSPIVProjected with random start")
+                int_opt_T =ITensorCPD.optimize(cp_T,alsQR;verbose);
+                push!(SEQRCS_error_vect,check_fit(alsQR, int_opt_T.factors, r, int_opt_T.λ, 1))
+
+                factors = Vector{ITensor}()
+                lambdaa= nothing
+                for (i, Q) in enumerate(alsQR.additional_items[:qr_factors])
+                    mode_idx = inds(T)[i]
+                    fact = ITensors.itensor(Q[:,1:dim(r)],mode_idx,r)
+                    fact,lambda = ITensorCPD.row_norm(fact,mode_idx)
+                    push!(factors,fact)  
+                    lambdaa= lambda
+                end
+                cpd_HOSVD = ITensorCPD.CPD{ITensor}(factors, lambdaa)
+                alsQR = ITensorCPD.compute_als(T,cpd_HOSVD;alg=alg_QR, check =check_piv);
+                # println("Start SEQRCSPIVProjected with HOSVD start")
+                int_opt_T = ITensorCPD.optimize(cpd_HOSVD,alsQR;verbose);
+                push!(SEQRCS_error_vect_HOSVD,check_fit(alsQR, int_opt_T.factors, r, int_opt_T.λ, 1))
+
+                inds_T= inds(T)
+                lambdaa=nothing
+                factors = Vector{ITensor}()
+                for i in inds_T
+                    factor = high_coherence(dim(i),dim(r))
+                    fact = ITensors.itensor(factor,i,r)
+                    fact,lambda = ITensorCPD.row_norm(fact,i)
+                    mu, r_eff, row2 = coherence(array(fact); r = dim(r))
+                    println("mode $i: μ = $(round(mu, sigdigits=5)), r_eff = $r_eff")
+                    push!(factors,fact)
+                    lambdaa=lambda
+                end
+                cpd_High = ITensorCPD.CPD{ITensor}(factors, lambdaa)
+
+                alsQR = ITensorCPD.compute_als(T,cpd_High;alg=alg_QR, check =check_piv);
+                # println("Start SEQRCSPIVProjected with High_coh")
+                int_opt_T =ITensorCPD.optimize(cpd_High,alsQR;verbose);
+                push!(SEQRCS_error_vect_High,check_fit(alsQR, int_opt_T.factors, r, int_opt_T.λ, 1))
+
+            end
+        end
+
+        err_leverage = Vector{Float64}()
+        lev_error_vect = Vector{Float64}()
+        ## Leverage Score 
+        for s in samples
+            for q=1:nrepeats
+                alslev = ITensorCPD.compute_als(T,cp_T;alg = ITensorCPD.LevScoreSampled(s),check = check_piv)
+                int_opt_T = ITensorCPD.optimize(cp_T, alslev; verbose)
+                # println("Start lev")
+                push!(lev_error_vect,check_fit(alslev, int_opt_T.factors, r, int_opt_T.λ, 1))
+            end
+
+        end
+    
+        SEQRCS_error_mat = reshape(SEQRCS_error_vect, (nrepeats,length(samples)))
+        SEQRCS_error_mat_HOSVD = reshape(SEQRCS_error_vect_HOSVD, (nrepeats,length(samples)))
+        SEQRCS_error_mat_High = reshape(SEQRCS_error_vect_High, (nrepeats,length(samples)))
+        lev_error_mat = reshape(lev_error_vect, (nrepeats,length(samples)))
+        
+        err_SEQRCS = median.(eachcol(SEQRCS_error_mat))
+        err_SEQRCS_HOSVD = median.(eachcol(SEQRCS_error_mat_HOSVD))
+        err_SEQRCS_High = median.(eachcol(SEQRCS_error_mat_High))
+        err_leverage = median.(eachcol(lev_error_mat))
+
+        err_direct = Vector{Float64}()
+        alsNormal = ITensorCPD.compute_als(T, cp_T; alg=ITensorCPD.direct(), check =check_direct)
+        opt_T = ITensorCPD.optimize(cp_T, alsNormal; verbose)
+        direct_error = check_fit(alsNormal, opt_T.factors, r, opt_T.λ, 1)
+        push!(err_direct,direct_error)
+
+        plt2 = plot(samples[2:end], err_SEQRCS[2:end], marker=:o, label="SEQRCSPivProjected")
+        plot!(samples[2:end], err_SEQRCS_HOSVD[2:end], marker=:o, label="SEQRCSPivProjected_HOSVD")
+        plot!(samples[2:end], err_SEQRCS_High[2:end], marker=:o, label="SEQRCSPivProjected_High")
+        plot!(samples[2:end], err_leverage[2:end], marker=:o, label="LevScoreSampled")
+        plot!(samples[2:end], direct_error .* ones(length(samples[2:end])), marker=:o, label="Direct")
+
+        xlabel!("Number of Samples")
+        ylabel!("Error in CPD Fit")
+        title!("Synthetic Tensor Test:\n Rank $rk")
+
+
+        n = nothing
+        if elt == Float64
+            n = "F64"
+        else
+            n = "F32"
+        end
+        # savefig("$(@__DIR__)/../plots/synthetic_tensor/rank_$(rk)_test_$(n)_colin_$(c).pdf")
+        display(plt2)
+
+        #Violin plot of synthetic tensor
+        df = DataFrame(
+            sample = vcat(repeat(samples, inner=nrepeats), repeat(samples, inner=nrepeats)),
+            method = vcat(fill("SE-QRCS", nrepeats*length(samples)),
+                        fill("Leverage", nrepeats*length(samples))),
+            error  = vcat(SEQRCS_error_vect, lev_error_vect)
+        )
+
+        df.sample = CategoricalArray(string.(df.sample), ordered=true, levels=string.(samples))
+
+        df_SEQRCS = filter(row -> row.method == "SE-QRCS", df)
+        df_lev  = filter(row -> row.method == "Leverage", df)
+
+        p = @df df_SEQRCS violin(:sample, :error, side=:left, label="SE-QRCS", color=:blue, yscale=:log10)
+        @df df_lev  violin!(:sample, :error, side=:right, label="Leverage", color=:orange)
+        xlabel!("Sampling size")
+        ylabel!("Error distribution in CPD fit")
+        title!("Synthetic tensor test: \n Rank $rk")
+        legend=:topleft
+        display(p)
+
+    end
+end
