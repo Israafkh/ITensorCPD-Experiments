edb249d69c0ec0d7e1668db6b90b17ecb5e9bf52
Karl Pierce <karl.m.pierce@gmail.com>
2026-01-12 16:48:48 -0500

    Use lambda weights as defined by paper

--- test/synthetic/table_calculations.jl
+++ test/synthetic/table_calculations.jl
@@ -7,7 +7,10 @@ elt = Float64
 
 c = 0.9
 v = 0.1
-A, cp = Colinearity_Tensor(5, (400,400,400), elt(c), nothing, elt);
+r = 5
+lng = 100000
+nn = range(0.2, 0.8, length=lng)
+A, cp = Colinearity_Tensor(r, (400,400,400), elt(c), [nn[rand(1:lng)] for i in 1:r], elt);
 ind_A = inds(A)  
 dims = dim.(ind_A);
 C = randn(dims...);
@@ -27,21 +30,29 @@ err_SEQRCS = Vector{Float64}()
 
 opt = ITensorCPD.decompose(T, r; check=check_direct, verbose=true);
 
+verbose=false
 for q = 1:nrepeats
     alg = ITensorCPD.SEQRCSPivProjected(1, 80, (1,2,3), (5,))
     # alg = ITensorCPD.LevScoreSampled(80)
     alsQR = ITensorCPD.compute_als(T,cp_T;alg, check =check_piv);
+    alsQR.target .= T
     int_opt_T =
     ITensorCPD.optimize(cp_T,alsQR;verbose);
     check_fit(alsQR, int_opt_T)
     push!(SEQRCS_error_vect,check_fit(alsQR, int_opt_T))
 end
 
+# m = [
+#     (630, 677, 674) #0.5 0.01
+#     (666, 690, 704) #0.9 0.01
+#     (670, 645, 689) #0.5 0.1
+#     (612, 669, 676) #0.9 0.1
+# ]
 m = [
-    (630, 677, 674) #0.5 0.01
-    (666, 690, 704) #0.9 0.01
-    (670, 645, 689) #0.5 0.1
-    (612, 669, 676) #0.9 0.1
+    (745, 677, 656) # 0.5, 0.01
+    (673, 668, 671) # 0.9 0.01
+    (703, 710, 653) # 0.5, 0.1
+    (715, 728, 693) # 0.9, 0.1
 ]
 
 mean.(m)
