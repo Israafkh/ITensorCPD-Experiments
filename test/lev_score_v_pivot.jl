2d33695ae13581f05d936a54b5344cbdfe790980
Karl Pierce <karl.m.pierce@gmail.com>
2026-02-19 11:28:16 -0500

    Update lev_score_v_pivot figures to be more readable

--- test/lev_score_v_pivot.jl
+++ test/lev_score_v_pivot.jl
@@ -1,23 +1,30 @@
 # This is to check to see how the pivots compare to the positions of the leverage scores in the QR of a random tensor
 include("test_env.jl")
 
-using LinearAlgebra, AppleAccelerate, Plots, Random, Revise
+using LinearAlgebra, Plots, Random, Revise
 elt = Float64
 
 ########
 # order 3 mode 1
 ########
 c = 0.8
-i,j,k = 500,500,500
-#i,j,k = 531,308,640
+# i,j,k = 500,500,500
+i,j,k = 531,308,640
 #A, cp = Colinearity_Tensor(25, (i,j,k), elt(c), nothing, elt)
 #r = ITensorCPD.cp_rank(cp)
 #A = random_itensor(elt, Index.((i,j,k)))
 # cp = ITensorCPD.random_CPD(A, 5)
 # r = Index(10, "CPRank")
-bad = 2
-r = 10
-cpd = construct_large_lev_score_cpd((i,j,k), r, bad);
+bad = 10
+r = 70
+# cpd = construct_large_lev_score_cpd((i,j,k), r, bad);
+cpd = ITensorCPD.random_CPD(zeros(i,j,k), r);
+array(cpd[1])[1:bad, :] .*= 100;
+array(cpd[2])[1:bad, :] .*= 100;
+array(cpd[3])[1:bad, :] .*= 100;
+array(cpd[1]) .= array(cpd[1])[randperm(i), :]
+array(cpd[2]) .= array(cpd[2])[randperm(j), :]
+array(cpd[3]) .= array(cpd[3])[randperm(k), :]
 r = ITensorCPD.cp_rank(cpd);
 T = had_contract(cpd[1], cpd[2], r) * had_contract(cpd[3], cpd[], r)
 
@@ -29,9 +36,15 @@ Tm = compute_lev_score(krpm)
 
 rel = Tm ./ maximum(Tm)
 sum(i > 0.1 for i in rel)
-plot(sort(Tm; rev=true)[1:50], marker=:star, label="Sorted Scores")
-plot!((Tm[pT][1:50];), label="Pivoted QR Ordering")
-plot!(title="Sorted Leverage Scores of the KRP",ylabel="Leverage Score Value", xlabel="Sorted Position")
+p = plot(sort(Tm; rev=true)[1:120], label="Sorted Scores", ms=8, lw=7)
+plot!((Tm[pT][1:120];), label="Pivoted QR Ordering", marker=:star, markersize=9, lw=4, alpha=0.8)
+plot!(title="Sorted Leverage Scores of the KRP",
+ylabel="Leverage Score Value", 
+xlabel="Sorted Position",
+labelfontsize=12,
+tickfontsize=10,
+legendfontsize=11,
+legend=dim(r)==10 ? :topright : :bottomleft)
 # plot!(yrange=[-0.01,0.5])
 
 savefig("plots/lev_score_order/mode_1_rank_($(i),$(j),$(k))_bad_$(bad)_rank_$(dim(r)).pdf")
@@ -59,9 +72,15 @@ _,_,pT = qr(reshape(array(target), (j, i*k)), ColumnNorm())
 krpm = reshape(array(ITensorCPD.had_contract(cpd[1], cpd[3], r)), (i * k , dim(r)))
 Tm = compute_lev_score(krpm)
 
-plot(sort(Tm; rev=true)[1:50], marker=:star, label="Exact Sorting")
-plot!((Tm[pT][1:50];), label="Pivoted QR Ordering")
-plot!(title="Sorted Leverage Scores of the KRP",ylabel="Leverage Score Value", xlabel="Sorted Position")
+p = plot(sort(Tm; rev=true)[1:120], label="Sorted Scores", ms=8, lw=7)
+plot!((Tm[pT][1:120];), label="Pivoted QR Ordering", marker=:star, markersize=9, lw=4, alpha=0.8, mws=6)
+plot!(title="Sorted Leverage Scores of the KRP",
+ylabel="Leverage Score Value", 
+xlabel="Sorted Position",
+labelfontsize=12,
+tickfontsize=10,
+legendfontsize=11,
+legend=dim(r)==10 ? :topright : :bottomleft)
 # plot!(yrange=[-0.01,0.5])
 
 savefig("plots/lev_score_order/mode_2_rank_($(i),$(j),$(k))_bad_$(bad)_rank_$(dim(r)).pdf")
