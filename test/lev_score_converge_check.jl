543b394834a7002718df004a13a49dbcfc4bc3c2
Karl Pierce <karl.m.pierce@gmail.com>
2026-02-25 09:52:23 -0500

    Update to test, more robust

--- test/lev_score_converge_check.jl
+++ test/lev_score_converge_check.jl
@@ -25,32 +25,38 @@ i,j,k = 90,90,90
 samps = 2000
 bad = 10
 r = 50
-# cpd_exact = ITensorCPD.random_CPD(zeros(i,j,k), r);
-# array(cpd_exact[1])[1:bad, :] .*= 100;
-# array(cpd_exact[2])[1:bad, :] .*= 100;
-# array(cpd_exact[3])[1:bad, :] .*= 100;
-# array(cpd_exact[1]) .= array(cpd_exact[1])[randperm(i), :]
-# array(cpd_exact[2]) .= array(cpd_exact[2])[randperm(j), :]
-# array(cpd_exact[3]) .= array(cpd_exact[3])[randperm(k), :]
-#  r = ITensorCPD.cp_rank(cpd_exact);
+cpd_exact = ITensorCPD.random_CPD(zeros(i,j,k), r);
+for (x, is) in zip(1:3, [i,j,k])
+    array(cpd_exact[x])[1:bad, :] .*= 20;
+    array(cpd_exact[x]) .= array(cpd_exact[x])[randperm(is), :]
+    tmp, lam = ITensorCPD.row_norm(cpd_exact[x], ind(cpd_exact[x], 1))
+    array(cpd_exact[x]) .= tmp
+    cpd_exact[] .= lam
+end
 
-seed = Xoshiro(123)
-rng = RandomDevice()
-cpd_exact = construct_large_lev_score_cpd((i,j,k), r, bad;rng);
-r = ITensorCPD.cp_rank(cpd_exact)
+ r = ITensorCPD.cp_rank(cpd_exact);
+
+# seed = Xoshiro(123)
+# rng = RandomDevice()
+# cpd_exact = construct_large_lev_score_cpd((i,j,k), r, bad;rng);
+# r = ITensorCPD.cp_rank(cpd_exact)
 
 T = had_contract(cpd_exact[1], cpd_exact[2], r) * had_contract(cpd_exact[3], cpd_exact[], r)
 
-als = ITensorCPD.compute_als(T, cpd_exact; alg=ITensorCPD.InvKRP(), check=ITensorCPD.CPAngleCheck(-1, 10,), trunc_tol=1e-10,normal=false);
-cpd_exact = ITensorCPD.optimize(cpd_exact, als; verbose=true);
+# als = ITensorCPD.compute_als(T, cpd_exact; alg=ITensorCPD.InvKRP(), check=ITensorCPD.CPAngleCheck(-1, 10,), trunc_tol=1e-10,normal=false);
+# cpd_exact = ITensorCPD.optimize(cpd_exact, als; verbose=true);
 
 cpd = ITensorCPD.random_CPD(T, dim(r); rng=RandomDevice());
 check=ITensorCPD.FitCheck(1e-5, 100, norm(T))
-check_piv = ITensorCPD.CPAngleCheck(1e-8, 500, )
-# alg = ITensorCPD.SEQRCSPivProjected(1, 50, (1,2,3), 40)
+check_piv = ITensorCPD.CPAngleCheck(1e-8, 5, )
+
 alg = ITensorCPD.direct()
-als = ITensorCPD.compute_als(T, cpd; alg, check=check_piv,normal=false);
-int_opt_T = ITensorCPD.optimize(cpd,als;verbose=true);
+als = ITensorCPD.compute_als(T, cpd; alg, check=check_piv, normal=false);
+## Choose to update only the factors in the KRP
+int_opt_T, als, _ = single_solve(als, cpd, 2);
+int_opt_T, als, _ = single_solve(als, int_opt_T, 3);
+## Or run a small number of LS updates
+# int_opt_T = ITensorCPD.optimize(cpd,als;verbose=true);
 als.target .= T
 check_fit(T, int_opt_T)
 
@@ -58,31 +64,44 @@ samps = 1000
 alg = ITensorCPD.SEQRCSPivProjected(1, samps, (1,2,3), 40)
 upals = ITensorCPD.compute_als(T, cpd; alg, check=check_piv, normal=true);
 upals.target .= T
-upcpd = ITensorCPD.optimize(cpd, upals; verbose=true);
+## Choose to update only the factors in the KRP
+upcpd, upals, _ = single_solve(upals, cpd, 2);
+upcpd, upals, _ = single_solve(upals, upcpd, 3);
+## Or run a small number of LS updates
+# upcpd = ITensorCPD.optimize(cpd, upals; verbose=true);
+
 check_fit(T, upcpd)
+
+
 
 W1 = ITensorCPD.had_contract(cpd_exact[2], cpd_exact[3], r)
 levW1 = compute_lev_score(copy(reshape(array(W1), (90*90,dim(r)))))
-W2 = ITensorCPD.had_contract(int_opt_T[2], int_opt_T[3], ITensorCPD.cp_rank(int_opt_T))
+W2 = ITensorCPD.had_contract(cpd[2], cpd[3], ITensorCPD.cp_rank(cpd))
 levW2 = compute_lev_score(copy(reshape(array(W2), (90*90,dim(r)))))
-W3 = ITensorCPD.had_contract(upcpd[2], upcpd[3], ITensorCPD.cp_rank(upcpd))
+W3 = ITensorCPD.had_contract(int_opt_T[2], int_opt_T[3], ITensorCPD.cp_rank(int_opt_T))
 levW3 = compute_lev_score(copy(reshape(array(W3), (90*90,dim(r)))))
+W4 = ITensorCPD.had_contract(upcpd[2], upcpd[3], ITensorCPD.cp_rank(upcpd))
+levW4 = compute_lev_score(copy(reshape(array(W4), (90*90,dim(r)))))
 
-angle_true_normal = acos(dot(levW1, levW2) / (norm(levW1) * norm(levW2))) 
-angle_true_qr = acos(dot(levW1, levW3) / (norm(levW1) * norm(levW3))) 
+angle_true_rand = acos(dot(levW1, levW2) / (norm(levW1) * norm(levW2))) 
+angle_true_normal = acos(dot(levW1, levW3) / (norm(levW1) * norm(levW3))) 
+angle_true_qr = acos(dot(levW1, levW4) / (norm(levW1) * norm(levW4))) 
+println("The angle between the true KRP and the random guess is $(angle_true_rand)")
 println("The angle between the true KRP and the normal equations is $(angle_true_normal)")
 println("The angle between the true KRP and the QR-random LS is $(angle_true_qr)")
 
 p = plot(sort(levW1; rev=true)[1:1000]; label="Exact CPD")
-plot!(sort(levW2; rev=true)[1:1000]; label="Normal Equation")
-plot!(sort(levW3;rev=true)[1:1000]; label="SE-QRCS with 10 samples")
+plot!(sort(levW2; rev=true)[1:1000]; label="Random Guess")
+plot!(sort(levW3;rev=true)[1:1000]; label="Normal Equations")
+plot!(sort(levW4;rev=true)[1:1000]; label="QR random ")
 plot!(title="CP rank = 50, bad = 10", ylabel="Sorted leverage scores", xlabel="Leverage score number")
 
 display(p)
 # savefig("Levs.pdf")
 
-p = plot(levW1[1:100], label="True KRP", marker=:circle)
-plot!(levW2[1:100], label="Normal Equations")
-plot!(levW3[1:100]; alpha = 0.8, label="QR Random")
+p = plot(levW1[400:500], label="True KRP", marker=:circle)
+plot!(levW2[400:500], label="Normal Equations")
+plot!(levW3[400:500]; marker=:square, label="Normal")
+plot!(levW4[400:500]; marker=:diamond, label="QR Random")
 
 display(p)
